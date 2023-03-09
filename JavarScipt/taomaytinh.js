2285fa403857920d2c3d93019099173b3d137fe0
TuanVu0929 <tranngoc.haidang0402@gmail.com>
2023-03-09 08:21:43 +0700

    thêm file tinh toán

---
+++ JavarScipt/taomaytinh.js
@@ -0,0 +1,39 @@
+function Cong() {
+    let A = document.getElementById("A").value;
+    let B = document.getElementById("B").value;
+    A = parseInt(document.getElementById("A").value)
+    B = parseInt(document.getElementById("B").value)
+    let cong = A + B;
+    alert(cong);
+}
+function Tru() {
+    let A = document.getElementById("A").value;
+    let B = document.getElementById("B").value;
+    A = parseInt(document.getElementById("A").value)
+    B = parseInt(document.getElementById("B").value)
+    let tru = A - B;
+    alert(tru);
+}
+function Nhan() {
+    let A = document.getElementById("A").value;
+    let B = document.getElementById("B").value;
+    A = parseInt(document.getElementById("A").value)
+    B = parseInt(document.getElementById("B").value)
+    let nhan = A * B;
+    alert(nhan);
+}
+function Chia(){
+    let A = document.getElementById("A").value;
+    let B = document.getElementById("B").value;
+     A= parseInt(document.getElementById("A").value)
+     B =parseInt(document.getElementById("B").value)
+    let chia = A/B;
+    alert(chia);
+}
+
+
+
+
+
+
+
